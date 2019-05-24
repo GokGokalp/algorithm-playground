7aae0faf91345b64e6fd5f65cd95215e19ad2f2f
Gokhan Gokalp <ggokalp@boynergrup.com>
2019-05-24 16:26:32 +0300

    reverse string

---
+++ ReverseString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello World";
+
+            string reversedInput = Reverse(input);
+
+            Console.WriteLine(reversedInput);
+            Console.ReadKey();
+        }
+
+        static string Reverse(string input)
+        {
+            // Time complexity O(N)
+            string[] inputArr = input.Split(" ");
+            string reversedInput = string.Empty;
+
+            for (int i = inputArr.Length -1; i >= 0; i--)
+            {
+                reversedInput += inputArr[i] + " ";
+            }
+
+            reversedInput = reversedInput.Substring(0, reversedInput.Length -1);
+            return reversedInput;
+        }
+    }
+}
---
+++ ReverseString/README.md
@@ -0,0 +1,3 @@
+#### Problem:
+
+Write a short program that prints words of a string in reverse order.
