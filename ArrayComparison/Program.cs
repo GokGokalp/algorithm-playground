a3ec247a520c063436a166bd66e18b7e239f785e
Gokhan Gokalp <ggokalp@boynergrup.com>
2019-06-12 10:19:43 +0300

    Array comparison added

---
+++ ArrayComparison/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 3, 1, 2, 4 };
+            int[] b = new int[] { 1, 2, 5 };
+
+            int[] output = GetCommonValues(a, b);
+
+            Console.WriteLine(string.Join(',', Array.ConvertAll(output, c => c.ToString())));
+        }
+
+        static int[] GetCommonValues(int[] a, int[] b)
+        {
+            // Time complexity is O(n^2)
+            int[] output = new int[a.Length]; // It can be List<int>
+            int counter = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int y = 0; y < b.Length; y++)
+                {
+                    if (b[y] == a[i])
+                    {
+                        output[counter] = b[y];
+                        counter++;
+                    }
+                }
+            }
+
+            Array.Resize<int>(ref output, counter);
+
+            return output;
+        }
+    }
+}
---
+++ ArrayComparison/README.md
@@ -0,0 +1,5 @@
+#### Problem:
+
+Write a short program that compares two arrays and creates another array which holds the common values between two array.
+
+E.g: A={3,1,2,4} B={1,2,5} and C={1,2}
